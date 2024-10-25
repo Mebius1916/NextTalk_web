827056700e9ca489587f0bc029c025a2c0e8a9df
mebius <591626940@qq.com>
2024-10-25 23:51:59 +0800

    updateAgain

--- pages/api/createMessage.ts
+++ pages/api/createMessage.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async function createMessage(req: NextApiRequest, res: NextApiResponse) {
   const { messages } = req.body;
   const apiKey = process.env.OPEN_AI_API_KEY;
-  const url = process.env.OPEN_AI_API;
+  const url = "https://chatapi.littlewheat.com/v1/chat/completions";
   const body = JSON.stringify({
     messages,
     model: 'gpt-3.5-turbo',
